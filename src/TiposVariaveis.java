4506483279f475d6a14d5ddbd49b73c788247761
AlanoNovaes <109998485+AlanoNovaes@users.noreply.github.com>
2023-09-30 23:07:56 -0300

    alteracao

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -8,6 +8,7 @@ public class TiposVariaveis {
 		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
 		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
 		float pi = 3.14F;
-		double salario = 1275.33;
+		double salario = 1275.33; 
+        String nomeCompleto = "Linguagem" + "Java";
     }
 }
